3e1590c0863e0a3e22bf69418ae39e4e789e265c
Laurent Martelli <laurent@bearteam.org>
2019-12-17 08:48:55 +0100

    2015 day 3

---
+++ src/aoc_2015/day03.clj
@@ -0,0 +1,44 @@
+(ns aoc-2015.day03
+  (:require [aoc.core :refer :all]
+            [clojure.string :refer [split]]))
+
+(def puzzle-input (puzzle-input-string))
+
+;; part 1
+
+(defn move [pos dir]
+     (case dir
+       \< (update pos 0 dec)
+       \> (update pos 0 inc)
+       \^ (update pos 1 dec)
+       \v (update pos 1 inc)))
+
+(defn distribute [directions]
+  (reduce
+       (fn [{pos :pos visited :visited} dir]
+         (let [new-pos (move pos dir)]
+           {:pos new-pos
+            :visited (conj visited new-pos)}))
+       {:pos [0 0] :visited #{[0 0]}}
+       directions))
+
+(defn part1 [input]
+  (-> (distribute input)
+      :visited
+      count))
+
+;; part 2
+
+(defn dispatch [coll n]
+  (remove-nil
+   (reduce
+    (fn [colls items] (doall (map conj colls items)))
+    (repeat n [])
+    (partition n n (repeat nil) coll))))
+
+(defn part2 [input]
+  (->> (for [directions (dispatch input 2)]
+        (-> (distribute directions)
+            :visited))
+       (reduce into #{})
+       count))
