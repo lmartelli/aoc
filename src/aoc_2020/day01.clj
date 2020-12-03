f00f9a186100aa51119b1e8b51f3c4cbe204bf9e
Laurent Martelli <laurent@bearteam.org>
2020-12-04 00:40:57 +0100

    2020 day 1 : use combinatorics/combinations

--- src/aoc_2020/day01.clj
+++ src/aoc_2020/day01.clj
@@ -1,27 +1,26 @@
 (ns aoc-2020.day01
   (:require
    [aoc.core :refer :all]
-   [clojure.test :refer :all]))
+   [clojure.test :refer :all]
+   [clojure.math.combinatorics :refer [combinations]]))
 
 (puzzle-input-parse-lines parse-int)
 
 ;; part 1
 
+(defn solve [input n]
+  (->> (combinations input n)
+       (filter #(= 2020 (apply + %)))
+       (map #(apply * %))
+       first))
+
+(defn solve2 [input]
+  (for [x input y input z input :when (= 2020 (+ x y z))] (* x y z)))
+
 (defpart part1 [input]
-  (first
-   (for [x input
-         y input
-         :let [s (+ x y)]
-         :when (= s 2020)]
-     (* x y))))
+  (solve input 2))
 
 ;; part 2
 
 (defpart part2 [input]
-  (first
-   (for [x input
-         y input
-         z input
-         :let [s (+ x y z)]
-         :when (= s 2020)]
-     (* x y z))))
+  (solve input 3))
