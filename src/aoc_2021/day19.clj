9aa7907c6a6bf8012d3156271efc982ccf483c96
Laurent Martelli <martellilaurent@gmail.com>
2022-12-04 20:24:08 +0100

    2021 day 19: optimization

--- src/aoc_2021/day19.clj
+++ src/aoc_2021/day19.clj
@@ -42,20 +42,38 @@
 (defn relative-to-each [points]
   (map #(relative-to % points) points))
 
+(defn intersection-count-greater-than-or-equal [a b n]
+  (let [set-a (into #{} a)]
+    (and
+      (>= (count b) n)
+      (reduce
+        (fn [[found remaining] x]
+          (if (set-a x)
+            (if (= (inc found) n)
+              (reduced true)
+              [(inc found) (dec remaining)])
+            (if (< remaining (- n found))
+              (reduced false)
+              [found (dec remaining)])))
+        [0 (dec (count b))]
+        b)
+      )))
+
 (defn overlapping-beacons
   [beacons-a beacons-b]
   (->> (for [a (->> (orientations beacons-a)
                     (mapcat #(relative-to-each %)))
              b (relative-to-each beacons-b)]
          [a b])
-       (find-first (fn [[a b]] (>= (count (intersection (into #{} a) (into #{} b))) 12)))))
+       (find-first (fn [[a b]] (intersection-count-greater-than-or-equal a b 12)))))
 
 (defn find-overlapping-pair [scans]
   (-> (for [[a b] (combinations scans 2)
             :let [overlap (overlapping-beacons a b)
                   [overlap-a overlap-b] overlap]
             :when overlap]
-        (let [found (conj (filter (complement #{a b}) scans) (into #{} (concat overlap-a overlap-b)))]
+        (let [found (conj (filter (complement #{a b}) scans)
+                          (into #{} (concat overlap-a overlap-b)))]
           (println "Found overlapping pair")
           ;;(clojure.pprint/pprint found)
           found))
@@ -78,6 +96,16 @@
 ;; tests
 
 (def test-data (puzzle-input (test-input *ns*)))
+
+(deftest intersection-count-greater-than-test
+  (are [a b n expected] (= expected (intersection-count-greater-than-or-equal a b n))
+    [1] [2] 1 false
+    [1] [1] 1 true
+    [2 1] [1] 1 true
+    [2 1] [1] 2 false
+    [2 1] [3 1] 1 true
+    [1 2 3 4] [7 5 4 9 1 8 2 6] 3 true
+    ))
 
 (deftest part1-test
   (is (= 79 (part1 test-data))))
