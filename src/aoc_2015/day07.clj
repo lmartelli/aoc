fb5d412a612046eac7e0c6b1a4fc747cedb27842
Laurent MARTELLI <laurent.martelli@devoteam.com>
2022-12-06 00:28:59 +0100

    ♻ rewrite 2015 day 7 in pure functional style

--- src/aoc_2015/day07.clj
+++ src/aoc_2015/day07.clj
@@ -1,65 +1,71 @@
 (ns aoc-2015.day07
   (:require
    [aoc.core :refer :all]
-   [clojure.string :refer [split]]))
+   [clojure.string :refer [split]]
+   [clojure.test :refer :all]))
 
 (defn convert-token [t]
   (if (re-matches #"\d+" t)
     (parse-int t)
-    (symbol t)))
+    (keyword t)))
 
-(puzzle-input-parse-lines
- (fn [line]
-   (let [[expr name] (split line #" -> ")]
-     (let [[a b c :as tokens] (->> (split expr #" ") (map convert-token))]
-       [(symbol name)
-        (case (count tokens)
-          1 a
-          2 (list a b)
-          3 (list b a c))]))))
+(defn puzzle-input [stream]
+  (puzzle-input-parse-lines
+   stream
+   (fn [line]
+     (let [[expr name] (split line #" -> ")]
+       (let [[a b c :as tokens] (->> (split expr #" ") (map convert-token))]
+         [(keyword name)
+          (case (count tokens)
+            1 a
+            2 (list a b)
+            3 (list b a c))])))))
 
 ;; part 1
 
-(def max-val 65535)
+(def MAX_VAL 65535)
 
-(defn eval [e bindings]
-  (cond
-    (list? e) (let [[op & params] e]
-                (apply (resolve op) (map #(eval % bindings) params)))
-    (int? e) e
-    (symbol? e) (let [def (@bindings e)]
-                  (let [res (eval def bindings)]
-                    (swap! bindings assoc e res)
-                    res))))
+(def operators
+  {:OR bit-or
+   :AND bit-and
+   :NOT #(- MAX_VAL %)
+   :LSHIFT #(bit-and MAX_VAL (bit-shift-left %1 %2))
+   :RSHIFT bit-shift-right})
 
-(defn RSHIFT [val n]
-  (bit-shift-right val n))
-
-(defn LSHIFT [val n]
-  (bit-and max-val (bit-shift-left val n)))
-
-(defn OR [a b]
-  (bit-or a b))
-
-(defn AND [a b]
-  (bit-and a b))
-
-(defn NOT [a]
-  (- max-val a))
+(def eval
+  (memoize
+   (fn [e bindings]
+     ;;(println e)
+     (cond
+       (list? e) (let [[op & params] e]
+                   (apply (operators op) (map #(eval % bindings) params)))
+       (int? e) e
+       (keyword? e) (eval (bindings e) bindings)))))
 
 (defn make-bindings [input]
-  (->> input (into {}) atom))
+  (into {} input))
 
 (defpart part1 [input]
   (let [bindings (make-bindings input)]
-    (eval 'a bindings)))
+    (eval :a bindings)))
 
 ;; part 2
 
-(defpart part2 [rules]
-  (let [a (part1 rules)
-        bindings (make-bindings rules)]
-    (swap! bindings assoc 'b a)
-    (eval 'a bindings)))
+(defpart part2 [input]
+  (let [bindings (make-bindings input)
+        a (eval :a bindings)]
+    (eval :a (assoc bindings :b a))))
 
 ;; tests
+
+(deftest eval-test
+  (let [bindings (make-bindings (puzzle-input (test-input)))]
+    (are [wire expected] (= expected (eval wire bindings))
+      :d 72
+      :e 507
+      :f 492
+      :g 114
+      :h 65412
+      :i 65079
+      :x 123
+      :y 456)))
