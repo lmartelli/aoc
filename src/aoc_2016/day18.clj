03e9aef139698e7ec5e1b6d699b172c03f964831
Laurent MARTELLI <laurent.martelli@devoteam.com>
2023-01-03 05:30:08 +0100

    2016 day 18 : optimize

--- src/aoc_2016/day18.clj
+++ src/aoc_2016/day18.clj
@@ -9,29 +9,34 @@
 
 ;; part 1
 
-(def trap-rules
-  (map-keys
-    seq
-    {"..." \.
-     "^.." \^
-     ".^." \.
-     "^^." \^
-     "..^" \^
-     "^.^" \.
-     ".^^" \^
-     "^^^" \.}))
+(defn next-row [^booleans row]
+  (let [len (alength row)
+        new-row (boolean-array len)]
+    (aset-boolean new-row 0 true)
+    (aset-boolean new-row (dec len) true)
+    (loop [i 1]
+      (if (= i (unchecked-dec-int len))
+        new-row
+        (do
+          (aset-boolean new-row i (= (aget row (unchecked-dec-int i)) (aget row (unchecked-inc-int i))))
+          (recur (unchecked-inc-int i)))))))
 
-(defn next-row [row]
-  (->> (concat "." row ".")
-       (partition 3 1)
-       (mapv trap-rules)))
+(defn boolean-row [row]
+  (->> (map {\^ false \. true} (str \. row \.))
+       (into-array Boolean/TYPE)))
 
 (defn rows [initial-row]
-  (iterate next-row (seq initial-row)))
+  (iterate next-row (boolean-row initial-row)))
 
 (defn count-safe-tiles
-  ([row]
-   (count (filter #(= % \.) row)))
+  ([^booleans row]
+   (loop [i (-> (alength row) (- 2)),
+          nb-safe 0]
+     (if (= i 0)
+       nb-safe
+       (if (aget row i)
+         (recur (unchecked-dec-int i) (unchecked-inc-int nb-safe))
+         (recur (unchecked-dec-int i) nb-safe)))))
   ([initial-row n]
    (->> (rows initial-row)
         (take n)
@@ -49,13 +54,9 @@
 ;; tests
 
 (deftest next-row-test
-  (are [row expected] (= (seq expected) (next-row row))
+  (are [row expected] (= (boolean-row expected) (next-row (boolean-row row)))
     "..^^." ".^^^^"
     ".^^^^" "^^..^"))
 
 (deftest count-safe-tiles-test
   (is ( = 38 (count-safe-tiles ".^^.^.^^^^" 10))))
-
-(deftest part1-test ())
-
-(deftest part2-test)
