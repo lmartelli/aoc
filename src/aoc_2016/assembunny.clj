2d7a187288477745823402b7dced717c71e5ceac
Laurent MARTELLI <laurent.martelli@devoteam.com>
2023-01-04 14:08:32 +0100

    2016: refacto assembunny
    Prepare for reuse by day 23

---
+++ src/aoc_2016/assembunny.clj
@@ -0,0 +1,57 @@
+(ns aoc-2016.assembunny
+  (:require
+   [aoc.core :refer :all]
+   [clojure.string :as str]
+   [clojure.test :refer :all]))
+
+;; Input parsing
+
+(defn keyword-or-int [s]
+  (if (letter? (first s))
+    (keyword s)
+    (parse-int s)))
+
+(defn puzzle-input [stream]
+  (->> stream
+       line-seq
+       (mapv (fn [line]
+               (->> (str/split line #" +")
+                    (mapv keyword-or-int))))))
+
+;; Program running
+
+(defn eval-expr [registers expr]
+  (if (keyword? expr) (registers expr) expr))
+
+(defn non-jump-instr [f]
+  (fn [register & operands]
+    (-> (apply f register operands)
+        (update! :ip inc))))
+
+(defn update-register [f]
+  (fn [registers r] (update! registers r f)))
+
+(defn jump-if [pred]
+  (fn [registers value offset]
+    (update! registers :ip
+             #(+ % (if (pred (eval-expr registers value)) offset 1)))))
+
+(defn copy [registers value r]
+  (assoc! registers r (eval-expr registers value)))
+
+(def instruction-set
+  {:cpy (non-jump-instr copy)
+   :inc (non-jump-instr (update-register inc))
+   :dec (non-jump-instr (update-register dec))
+   :jnz (jump-if #(not= 0 %))})
+
+(defn exec-instr [registers [instr & operands] instruction-set]
+  (apply (instruction-set instr) registers operands))
+
+(defn run-prog [prog registers]
+  (loop [registers (transient registers)]
+    (if (not (<= 0 (registers :ip) (dec (count prog))))
+      (persistent! registers)
+      (recur (exec-instr registers (prog (registers :ip)) instruction-set)))))
+
+;; tests
--- src/aoc_2016/day12.clj
+++ src/aoc_2016/day12.clj
@@ -1,65 +1,14 @@
 (ns aoc-2016.day12
   (:require
    [aoc.core :refer :all]
-   [clojure.string :refer [split]]
+   [aoc-2016.assembunny :refer :all]
    [clojure.test :refer :all]))
 
-(defn keyword-or-int [s]
-  (if (letter? (first s))
-    (keyword s)
-    (parse-int s)))
-
-(defn puzzle-input [stream]
-  (->> stream
-       line-seq
-       (mapv (fn [line]
-               (->> (split line #" +")
-                    (mapv keyword-or-int))))))
+;; Use aoc-2016.assembunny/puzzle-input
 
 ;; part 1
 
-(defn eval [registers expr]
-  (if (keyword? expr) (registers expr) expr))
-
-(defn default-instr [f]
-  (fn [register & operands]
-    (-> (apply f register operands)
-        (update :ip inc))))
-
-(defn update-register [f]
-  (fn [registers r] (update registers r f)))
-
-(defn jump-if [pred]
-  (fn [registers value offset]
-    (update registers :ip
-            #(+ % (if (pred (eval registers value)) offset 1)))))
-
-(defn copy [registers value r]
-  (assoc registers r (eval registers value)))
-
-(def instructions
-  {:cpy (default-instr copy)
-   :inc (default-instr (update-register inc))
-   :dec (default-instr (update-register dec))
-   :jnz (jump-if #(not= 0 %))})
-
-(defn instruction [instr]
-  (let [i (instructions instr)]
-    (when-not i (throw (Exception. (str "no such instruction: " instr))))
-    i))
-
-(defn exec-instr [registers [instr & operands]]
-  ;;(apply println registers instr operands)
-  (apply (instruction instr) registers operands))
-
 (def init-registers (constantly {:a 0 :b 0 :c 0 :d 0 :ip 0}))
-
-(defn run-prog [prog registers]
-  (->> (iterate
-        (fn [registers]
-          (exec-instr registers (get prog (registers :ip))))
-        registers)
-       (find-first #(not (<= 0 (% :ip) (dec (count prog)))))))
 
 (defpart part1 [input]
   (-> (run-prog input (init-registers))
