401c98c7f87dd8d3f426f6d3e942f5d065034d1d
Laurent MARTELLI <laurent.martelli@devoteam.com>
2023-01-22 15:32:21 +0100

    2020 day 17

--- src/aoc_2020/day17.clj
+++ src/aoc_2020/day17.clj
@@ -3,64 +3,66 @@
    [aoc.core :refer :all]
    [aoc.space-2d :as s2]
    [aoc.space-3d :as s3]
+   [clojure.math.combinatorics :refer :all]
    [clojure.test :refer :all]))
 
 (def-input-parser [lines]
-  (->> (s2/parse-2d-map-positions lines)
-       (map #(vec (conj % 0)))
-       (into #{})))
+  (s2/parse-2d-map-positions lines))
 
 ;; part 1
 
-(defn count-neighbours [pos cubes neighbours]
-  (->> (neighbours pos)
-       (filter cubes)
-       count))
+(defn add-dimension [coords]
+  (map #(vec (conj % 0)) coords))
 
-(defn expand-range [[min max]]
-  [(dec min) (inc max)])
+(defn neighbours-n-dim [nb-dim]
+  (let [coord-modifiers (->> (apply cartesian-product (repeat nb-dim [identity inc dec]))
+                             (remove (eq (repeat nb-dim identity))))]
+    (fn [p]
+      (->> (map
+             #(mapv (fn [f x] (f x)) % p)
+             coord-modifiers)))))
 
-(defn cycle [cubes neighbours]
-  (let [ranges (s3/xyz-ranges cubes)]
-    (->> (s3/pos-in-ranges (map expand-range ranges))
-         (keep (fn [p]
-                 (let [nb-neighbours (count-neighbours p cubes neighbours)]
-                   (if (cubes p)
-                     (when (<= 2 nb-neighbours 3) p)
-                     (when (= 3 nb-neighbours) p)))))
-         (into #{}))))
+(defn exec-cycle [cubes neighbours]
+  (->> (mapcat neighbours cubes)
+       frequencies
+       (keep (fn [[p n]]
+               (if (cubes p)
+                 (when (<= 2 n 3) p)
+                 (when (= 3 n) p))))
+       (into #{})))  
 
-(defpart part1 [cubes]
-  (-> (iterate cycle cubes)
-      (nth 6)
-      count))
+(defn count-active-cubes-after-boot-process [init-plane n]
+  (let [cubes (-> (iterate add-dimension init-plane)
+                  (nth (- n 2))
+                  set)
+        neighbours (neighbours-n-dim n)]
+    (-> (iterate #(exec-cycle % neighbours) cubes)
+        (nth 6)
+        count)))
+
+(defpart part1 [init-plane]
+  (count-active-cubes-after-boot-process init-plane 3))
 
 ;; part 2
-
-(defmacro list-neighbours [x y z w]
-  (->> (for [X `[~x (inc ~x) (dec ~x)]
-             Y `[~y (inc ~y) (dec ~y)]
-             Z `[~z (inc ~z) (dec ~z)]
-             W `[~w (inc ~w) (dec ~w)]]
-         [X Y Z W])
-       (remove #(= % `[~x ~y ~z ~w]))
-       vec))
-
-(defpart part2 [cubes]
-)
+(defpart part2 [init-plane]
+  (count-active-cubes-after-boot-process init-plane 4))
 
 ;; tests
 
+(deftest neighbours-test
+  (testing "1 dimnension"
+    (is (= #{[-1] [1]} (set ((neighbours-n-dim 1) [0])))))
+  (testing "2 dimnensions"
+    (is (= #{[1 0] [1 1] [0 1] [-1 1] [-1 0] [-1 -1] [0 -1] [1 -1]}
+           (set ((neighbours-n-dim 2) [0 0]))))))
+
+(def data
+  [".#."
+   "..#"
+   "###"])
+
 (deftest part1-test
-  (test-with-lines
-    part1
-    [".#."
-     "..#"
-     "###"]
-    112))
+  (test-with-lines part1 data 112))
 
 (deftest part2-test
-  (test-with-lines
-    part2
-    [""]
-    nil))
+  (test-with-lines part2 data 848))
